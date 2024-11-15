0b34eb0d4efa6bfa1c9d6e849f7c84ac9eb9ccba
AxelsBernal <AxsandovalBe@ittepic.edu.mx>
2024-11-15 13:59:45 -0700

    imagen pagina principal

--- nutritalia/src/pages/Home.jsx
+++ nutritalia/src/pages/Home.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import '../css/Home.css';
+import imagen1 from '../images/SpaguetiBologesa.png'
+import imagen2 from '../images/PastaCremosa.png'
+import imagen3 from '../images/PastaCItaliana.png'
+import logo from '../images/logoLetra.png'; 
+
 
 function Home() {
   return (
@@ -17,14 +22,30 @@ function Home() {
     <br/>
     <br/>
       <section className="seasonal-specials-section">
-
+    
         <h2>~ ESPECIALES DE TEMPORADA ~</h2>
       </section>
+
+         {/* Sección de Imágenes */}
+         <section className="image-gallery">
+        <img src={imagen1} alt="Especialidad 1" className="gallery-image" />
+        <img src={imagen2}  alt="Especialidad 2" className="gallery-image" />
+        <img src={imagen3}  alt="Especialidad 3" className="gallery-image" />
+      </section>
+
+      <br/>
+      <br/>
+      <br/>
+
+      <div className="navbar-logo">
+        <img src={logo} alt="Logo de Nutritalia" />
+      </div>
 
       {/* Segunda sección con color de fondo */}
       <section className="content-section">
         <p>Contenido adicional aquí...</p>
       </section>
+      <div className="line"></div>
     </div>
   );
 }
